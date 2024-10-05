153c42387f8272fdd615797b94fbf6374a9fdab1
flopau76 <flopau76210@gmail.com>
2024-10-05 15:51:50 +0200

    code completion

--- TP/__main__.py
+++ TP/__main__.py
@@ -1,28 +1,54 @@
 from TP.loading import load_directory
-from TP.kmers import stream_kmers, kmer2str
+from TP.kmers import stream_kmers, make_dict
+from time import time
 
+def jaccard(fileA, fileB, k):   # as seen in lecture
+    """ Computes the Jaccard similarity between two list of sequences """
+    dictA = make_dict(fileA, k)
+    intersection = 0
+    union = sum(dictA.values())
+    for kmer in stream_kmers(fileB, k):
+        if kmer in dictA and dictA[kmer] > 0:
+            intersection += 1
+            dictA[kmer] -= 1
+        else:
+            union += 1
+    return intersection / union
 
-
-def jaccard(fileA, fileB, k):
-    j = 0
-    # --- To complete ---
-    return j
-
+def dict_intersection(dictA:dict, dictB:dict):
+    """ Computes the intersection of two dictionaries """
+    intersection = 0
+    for kmer, countB in dictB.items():
+        countA = dictA.get(kmer, 0)
+        intersection += min(countA, countB)
+    return intersection
 
 
 if __name__ == "__main__":
-    print("Computation of Jaccard similarity between files")
-
     # Load all the files in a dictionary
     files = load_directory("data")
+    filenames = list(files.keys())
     k = 21
     
     print("Computing Jaccard similarity for all pairs of samples")
-    filenames = list(files.keys())
+    # start = time()
+    # print("    naive version")
+    # for i in range(len(files)):
+    #     for j in range(i+1, len(files)):
+    #         dist_j = jaccard(files[filenames[i]], files[filenames[j]], k)
+    #         print(filenames[i], filenames[j], dist_j)
+    # print("Time:", time()-start)
+    # print()
+
+    start = time()
+    print("    precomputing the dictionnaries")
+    dicts = [make_dict(files[filename], k) for filename in filenames]
+    dicts_size = [sum(d.values()) for d in dicts]
+    print("time:", time()-start)
+    print("    computing the pairwise similarities")
     for i in range(len(files)):
         for j in range(i+1, len(files)):
-            
-            # --- Complete here ---
-
-            j = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(filenames[i], filenames[j], j)
+            intersection = dict_intersection(dicts[i], dicts[j])
+            dist_j = intersection / (dicts_size[i] + dicts_size[j] - intersection)
+            print(filenames[i], filenames[j], dist_j)
+    print("total time:", time()-start)
--- TP/kmers.py
+++ TP/kmers.py
@@ -1,3 +1,60 @@
+def encode_nucl_old(letter):
+    """ Encodes a nucleotide on two bits """
+    encode = {'A': 0b00, 'C': 0b01, 'T': 0b10, 'G': 0b11}
+    return encode[letter]
+
+def encode_nucl_rev_old(letter):
+    """ Encodes the complementary of a nucleotide on two bits """
+    encode = {'T': 0b00, 'G': 0b01, 'A': 0b10, 'C': 0b11}
+    return encode[letter]
+
+def encode_nucl(letter):
+    """ Encodes a nucleotide on two bits
+    using the ascii code"""
+    return (ord(letter) >> 1) & 0b11
+
+def encode_nucl_rev(letter):
+    """ Encodes the complementary of a nucleotide on two bits
+    using the ascii code"""
+    return encode_nucl(letter) ^ 0b10
+
+def encode_kmer(seq, k):
+    """ Encodes the first k-mer, and its reverse complementary """
+    kmer = 0
+    rev_kmer = 0
+    for letter in seq[0:k]:
+        kmer <<= 2
+        rev_kmer >>= 2
+        kmer += encode_nucl(letter)
+        rev_kmer += encode_nucl_rev(letter) << (2*(k-1))
+    return kmer, rev_kmer
+
+def stream_kmers(file, k):
+    """ Enumerates all k-mers present in file (list of sequences)
+    Note: we keep only the lowest value of each kmer and its reverse complementary """
+    for seq in file:
+        mask = (1 << (2*k)) - 1 # to keep only the k rightmost nucleotides
+        kmer, rev_kmer  = encode_kmer(seq, k)
+        yield  min(kmer, rev_kmer)
+        for i in range(len(seq)-k):
+            kmer <<= 2
+            kmer &= mask    # bitwise AND
+            kmer += encode_nucl(seq[i+k])
+            rev_kmer >>= 2
+            rev_kmer += encode_nucl_rev(seq[i+k]) << (2*(k-1))
+            yield min(kmer, rev_kmer)
+
+def make_dict(file, k):
+    """ Creates a dictionary representing the multi-set of k-mers present in file
+    Note: file is a list of sequences"""
+    dict = {}
+    for kmer in stream_kmers(file, k):
+        if kmer in dict:
+            dict[kmer] += 1
+        else:
+            dict[kmer] = 1
+    return dict
+
 
 def kmer2str(val, k):
     """ Transform a kmer integer into a its string representation
@@ -13,8 +70,3 @@ def kmer2str(val, k):
 
     str_val.reverse()
     return "".join(str_val)
-
-
-def stream_kmers(text, k):
-    # --- To complete ---
-    pass
